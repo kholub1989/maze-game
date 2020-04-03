787ca5e6cf556cba96b58cae2b9068b75979df93
Krisztian Holub <kholub1989@gmail.com>
2020-04-03 10:56:22 -0400

    add guiding comments

--- index.js
+++ index.js
@@ -41,4 +41,17 @@ const horizontals = Array(cells - 1)
   .fill(null)
   .map(() => Array(cells).fill(false));
 
-console.log(grid);
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+const stepThroughCell = (row, column) => {
+  // If i have vsited the cell at [row, column], then return
+  // Mark this cell as being visited
+  // Assemble randomly-ordered list of neighbors
+  // For each neigbor...
+  // See if that neighbor is out of bounds
+  // If I have visited that neighbor, continue to next neighbor
+  // Remove a wall from either horizontals or verticals
+  // Visit that next cell
+};
+
+stepThroughCell(startRow, startColumn);
